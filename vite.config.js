8d99a35722b86c15969bbc7b65c77aaa09c068c9
xisain <husenabs232bgr@gmail.com>
2025-06-09 14:42:38 +0700

    Update API proxy target in Vite configuration to use production endpoint

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://api.sentinova.my.id',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
