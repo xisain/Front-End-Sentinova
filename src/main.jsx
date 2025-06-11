574c012c742ae9a2509ffd812f267d3b00540d8c
rey-ctrl <adityazaky335@gmail.com>
2025-06-11 19:13:08 +0700

    adding and revision

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { NotificationProvider } from './pages/NotificationContext';
+import { NotificationProvider } from './pages/flow/NotificationContext';
 import './index.css';
 import { ClerkProvider } from "@clerk/clerk-react";
 
